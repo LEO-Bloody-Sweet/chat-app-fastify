ac7ee9a69fb5cdd0b1216e9d603a3321e3b1f94d
LEO-Bloody-Sweet <145244508+LEO-Bloody-Sweet@users.noreply.github.com>
2025-04-03 17:53:24 +0530

    Add files via upload

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,3 @@
+export function Login() {
+  return <h1 className="text-red-500 text-2xl font-bold">Login</h1>
+}
---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,10 @@
+export function Signup() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-8 text-center">Sign Up</h1>
+      <form>
+        <label></label>
+      </form>
+    </>
+  )
+}
